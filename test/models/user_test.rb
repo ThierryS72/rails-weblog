c49627232ca2053005b59b4c6cda41af32a3f5d0
Thibaud <thibaud@thibaud.gg>
2017-06-08 18:18:49 +0000

    COMMIT Exercice2

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,12 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(admin: false, name: 'jane')
     assert_equal 'jane', user.display_name 
   end
+  
+  test '#password_lenght' do
+    user = User.new(password: '1234')
+    
+    assert_not user.valid?
+    assert_equal [:password], user.errors.keys
+  end
+  
 end
