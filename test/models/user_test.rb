e41df1338bc69565843acafb51c10a3fb2904e5f
Thibaud <thibaud@thibaud.gg>
2017-06-13 09:34:54 +0000

    mise à jour des tests (ex1-3)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#display_name when admin' do
+    user = User.new(admin: true, name: 'Bob')
+
+    assert_equal 'Bob (admin)', user.display_name
+  end
+
+  test '#display_name when not admin' do
+    user = User.new(admin: false, name: 'Bob')
+
+    assert_equal 'Bob', user.display_name
+  end
+  
+  test 'ensures that password length is min 8' do
+     user = User.new(password: '1234567')
+ 
+     assert_not user.valid?
+     assert_equal [:password], user.errors.keys
+   end
+
 end
